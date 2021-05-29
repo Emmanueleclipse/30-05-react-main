b52f9dba62fc529af9d7fc801a1ea914fff60a7f
Naim Lasker <naim-lasker>
2021-05-29 17:29:40 +0600

    merged with latest code

--- src/app/screens/patron/ProcessingRequest/index.js
+++ src/app/screens/patron/ProcessingRequest/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import logoLoader from '@images/logo-loader.svg';
 
-const PatronRequestPosting = () => {
+const ProcessingRequest = ({ title, paragraph1, paragraph2 }) => {
   const textClassName = 'font-gotham-book text-sm text-gray-500 text-center';
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="mb-16 flex flex-col items-center">
         <p className="font-gotham-medium text-blue-gray text-md mb-4">
-          Posting your request!
+          {title}
         </p>
-        <p className={textClassName}>We’ll notify you when Storks</p>
-        <p className={textClassName}> start bidding.</p>
+        <p className={textClassName}>{paragraph1}</p>
+        <p className={textClassName}>{paragraph2}</p>
         <img className="mt-16" src={logoLoader} alt="logo-loader" />
       </div>
     </div>
   );
 };
 
-export default PatronRequestPosting;
+ProcessingRequest.propTypes = {
+  title: PropTypes.string.isRequired,
+  paragraph1: PropTypes.string.isRequired,
+  paragraph2: PropTypes.string.isRequired,
+};
+
+export default ProcessingRequest;
