88d79c0f84ca919159a30f52448cc31b68e330a2
Naim Lasker <naim-lasker>
2021-05-29 17:47:45 +0600

    merged with latest code

--- src/app/hooks/mutations/all/useMutationMessage.js
+++
@@ -1,6 +0,0 @@
-import { createQueryHook } from '@utils/createHook';
-import endpoints from '@utils/endpoints';
-
-const useMutationMessage = createQueryHook('bidMessages', endpoints.bidMessages);
-
-export default useMutationMessage;
